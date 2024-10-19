735d65263bb85a3e5919a5615e54324b371b04b2
Ryan Flores <ryanflorres@gmail.com>
2024-10-19 12:23:43 -0700

    Add Project

---
+++ data/projectsData.ts
@@ -0,0 +1,90 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'GBJAM11 - Hopscape',
+    description: 'I go over my entire first-ever GameJam experience, and, spoiler alert, it was amazing!',
+    imgSrc: '/static/images/projects/hopscape.gif',
+    href: 'https://github.com/RyanFloresTT/GBJAM11',
+  },
+  {
+    title: 'Dimension Dissension',
+    description: 'A roguelike game that is inspired Risk of Rain!',
+    imgSrc: '/static/images/projects/dimdis.gif',
+    href: 'https://github.com/RyanFloresTT/Dimension-Dissension',
+  },
+  {
+    title: 'Crystal Chaos',
+    description: 'A tower defense game made in Unity. ',
+    imgSrc: '/static/images/projects/zombiesurv.gif',
+    href: 'https://github.com/RyanFloresTT/TowerDefenseGame',
+  },
+  {
+    title: 'Discord Bot - TrustyBot',
+    description: 'A Discord bot made by yours truly using C#. ',
+    imgSrc: '/static/images/projects/trustybot.png',
+    href: 'https://github.com/RyanFloresTT/TrustyBot',
+  },
+  {
+    title: 'Pong Remake',
+    description: 'A remake of the famous Pong game.',
+    imgSrc: '/static/images/projects/pong.png',
+    href: 'https://github.com/RyanFloresTT/Pong-Remake',
+  },
+  {
+    title: 'Wrong Bin!',
+    description: 'A mini-game where you AVOID trash at all costs!',
+    imgSrc: '/static/images/projects/wrongbin.jpg',
+    href: 'https://github.com/RyanFloresTT/WrongBin-MiniGame',
+  },
+  {
+    title: 'This Portfolio!',
+    description: 'The website you"re on was made by me!',
+    imgSrc: '/static/images/projects/aspnetportfolio.png',
+    href: 'https://github.com/RyanFloresTT/TrustyPortfolio',
+  },
+  {
+    title: 'Zombie Survival',
+    description: 'A tower-defense, zombie survival game where you can upgrade towers and move them around!',
+    imgSrc: '/static/images/projects/zombiesurvvvv.gif',
+    href: 'https://www.github.com/RyanFloresTT/TowerDefenseGame',
+  },
+  {
+    title: 'Kitchen Chaos',
+    description: 'A cooking simulator game!',
+    imgSrc: '/static/images/projects/kitchen.png',
+    href: 'https://github.com/RyanFloresTT/Kitchen-Chaos',
+  },
+  {
+    title: 'Sorting Algorithm Visualizer',
+    description: 'A Sorting Algorithm Visualizer made in C++ using SFML and ImGui.',
+    imgSrc: '/static/images/projects/sorting.gif',
+    href: 'https://github.com/RyanFloresTT/SortingAlgorithmVisualizer',
+  },
+  {
+    title: 'Super Hangman',
+    description: 'My very first game released on Steam!',
+    imgSrc: '/static/images/projects/shangman.jpg',
+    href: 'https://gitlab.robbyk.com/robby/super-hangman',
+  },
+  {
+    title: 'Project Shmup',
+    description: 'Another attempt at the Roguelike genre.',
+    imgSrc: '/static/images/projects/shmup.png',
+    href: 'https://chiseled-harrier-6f9.notion.site/Shmup-Code-Documentation-a472ac554476456baabfe83d99637f99?pvs=4',
+  },
+  {
+    title: 'E-Commerce Site',
+    description: 'A project I made for my capstone class!',
+    imgSrc: '/static/images/projects/jom.jpg',
+    href: 'https://github.com/RyanFloresTT',
+  },
+];
+
+
+export default projectsData
